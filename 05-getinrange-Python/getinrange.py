0c90a92ffce4c1e32a984df79d98f3839d794c6e
sai srinivas <srinivas191194@msitprogram.net>
2020-07-14 19:58:02 +0530

    Jul 14  19:58:02

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -5,3 +5,17 @@
 # or if x is greater than the upper bound, return the upper bound.
 
 	      
+def fun_getinrange(x, bound1, bound2):
+    # your code goes here
+    if(bound1 < x < bound2) or (bound2 < x < bound1): 
+        return x
+    elif (bound1 > bound2):
+        if(x < bound2):
+            return bound2
+        else:
+            return bound1
+    else: 
+        if(x < bound1): 
+            return bound1
+        else:
+            return bound2
