de0019f78c8365916c4dbbeb9c91ab1145cc704e
sai srinivas <srinivas191194@msitprogram.net>
2020-07-14 21:34:30 +0530

    Jul 14  21:34:30

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -4,7 +4,12 @@
 # assert(handToDice(214) == (2,1,4))
 # assert(handToDice(422) == (4,2,2))
 # Hint: You might find // and % useful here, and also getKthDigit().
-
+import math
 def handtodice(hand):
-	# your code goes here
-	pass
+    # your code goes here
+    my_data=[]
+    while(hand > 0):
+        data = hand %10
+        my_data.append(data)
+        hand = math.floor(hand/10)
+    return (my_data[2], my_data[1], my_data[0])
