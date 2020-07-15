62f14f138bf5b2408d5ea62c9289fe0379242d85
sai srinivas <srinivas191194@msitprogram.net>
2020-07-15 22:04:53 +0530

    Jul 15  22:04:53

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -10,9 +10,9 @@ def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	   distance1 = math.floor(math.sqrt((pow(x2-x1),2) + (pow(y2-y1),2)))
 	   distance2 = math.floor(math.sqrt((pow(x2-x3),2) + (pow(y2-y3),2)))
 	   distance3 = math.floor(math.sqrt((pow(x3-x1),2) + (pow(y3-y1),2)))
-	   distance1 = math.pow(distance1,2)
-	   distance2 = math.pow(distance2,2)
-	   distance3 = math.pow(distance3,2)
+	   distance1 = math.pow((distance1),2)
+	   distance2 = math.pow((distance2),2)
+	   distance3 = math.pow((distance3),2)
 	   if(distance1 == distance2+distance3 or distance2 == distance1+distance3 or distance3 == distance1+distance2):
 		   return True
 	   return False	   
