57360cf4d4007b0fe98f07622ac6ae49fcf4baf5
sai srinivas <srinivas191194@msitprogram.net>
2020-07-15 21:37:20 +0530

    Jul 15  21:37:20

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -6,8 +6,7 @@
 
 def isevenpositiveint(x):
 	# your code goes here
-	string = x
-	if((string.isnumeric()) and (x >= 0) and (x%2 == 0)):
+	if( x >= 0 and x%2 == 0):
 		return True
 	else:
 	    return False
