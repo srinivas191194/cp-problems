7fa04f1133098114e27265b82f170892df77a7e0
sai srinivas <srinivas191194@msitprogram.net>
2020-07-17 13:38:51 +0530

    Jul 17  13:38:50

--- 03-recursion-Java/recursion.java
+++ 03-recursion-Java/recursion.java
@@ -1,5 +1,9 @@
 class recursion {
 	public int get_fib(int value){
-	    return -1; 
+		if(value == 0)
+		  return 0;
+		if(value == 1)
+		  return 1;
+	    return get_fib(value)*get_fib(value-1) ; 
 	}
 }
