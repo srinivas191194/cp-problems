1614a5cd009397f5f71b9780bc8783aa9ac09927
sai srinivas <srinivas191194@msitprogram.net>
2020-07-17 22:16:55 +0530

    Jul 17  22:16:55

--- 07-set_kth_digit-Java/set_kth_digit.java
+++ 07-set_kth_digit-Java/set_kth_digit.java
@@ -4,10 +4,33 @@
 // # the kth digit replaced with d. Counting starts at 0 and goes right-to-left, 
 // # so the 0th digit is the rightmost digit. 
 
- 
-class set_kth_digit {
-	public int fun_set_kth_digit(int n, int k, int d){
-		// your code goes here
-			return 0;
-	}
+import java.util.*;
+public class set_kth_digit {
+    public int fun_set_kth_digit(int n, int k, int d){
+        // your code goes here
+        int result = n;
+        int count = -1;
+        int placeValue = 1;
+        while(n > 0){
+            int x = n %10;
+            x = x*placeValue;
+            count++;
+            if(count == k){
+               result = (d*placeValue)+(result - x); 
+               return result;
+            }
+            placeValue = placeValue*10;
+            n = n/10;
+        }
+        int temp =(int) Math.pow(10,k);
+        if(result > 0){
+          result = temp+result;
+          return result;
+        }
+        else{
+          result = -temp+result;
+          return result;
+        }
+    }
+    
 }
