1ef62c8c3faf79990b494e9d4e3a0d41b9067be6
sai srinivas <srinivas191194@msitprogram.net>
2020-07-19 23:28:44 +0530

    done

--- 06-largestperfectquare-Java/LargestPerfectSquare.java
+++ 06-largestperfectquare-Java/LargestPerfectSquare.java
@@ -6,10 +6,12 @@
 // assert(largestPerfectSquare(26) == 25)
 // Hint: you may wish to use a similar approach to how you solved isPerfectSquare on the hw.
 // Another hint: This can be written using just one or two lines of Python.
-
+import java.util.*;
+import java.lang.Math;
 public class LargestPerfectSquare {
 	public int largestPerfectSquare(int n) {
 		// Your code goes here
-		return 1;
+		int root =(int) Math.sqrt(n);
+		return root*root;
 	}
 }
