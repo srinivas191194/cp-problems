48bc87464c2ec20ed37af0238e3e26ec5e89af10
sai srinivas <srinivas191194@msitprogram.net>
2020-07-19 22:22:28 +0530

    Jul 19  22:22:28

--- 04-threelines_area-Java/threelines_area.java
+++ 04-threelines_area-Java/threelines_area.java
@@ -2,8 +2,10 @@
 // # that takes length of 3 sides
 // # find the area of a triangle(return an int) given its side lengths.
 import java.lang.Math.*;
+import java.util.*;
 class threelines_area {
 	public int fun_threelines_area(int a, int b, int c){
-		return 0;
+		double s = (a+b+c)/2;
+		return (int) Math.sqrt(s*(s-a)*(s-b)*(s-c));
 	}
 }
