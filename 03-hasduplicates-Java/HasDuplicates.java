9b4e5c3ddc98b585abbcaf7c826d2db3f626fc2d
sai srinivas <srinivas191194@msitprogram.net>
2020-07-20 17:01:25 +0530

    done

--- 03-hasduplicates-Java/HasDuplicates.java
+++ 03-hasduplicates-Java/HasDuplicates.java
@@ -2,10 +2,19 @@
 // Write the function hasDuplicates(L) that takes a 2d list L of arbitrary values, and returns True if L 
 // contains any duplicate values (that is, 
 // if any two values in L are equal to each other), and False otherwise.
-
+import java.util.*;
 public class HasDuplicates {
 	public boolean hasDuplicates(int[][] arr) {
 		// Your code goes here
+		HashSet<Integer> object = new HashSet<Integer>();
+		for(int i = 0 ; i < arr.length;i++){
+			for(int j = 0 ; j < arr.length;j++){
+				if(object.contains(arr[i][j]))
+					return true;
+				else  
+				   object.add(arr[i][j]);	
+			}
+		}
 		return false;
 	}
 }
