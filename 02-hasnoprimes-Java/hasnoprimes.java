4009f7477fc09ec6a4906b651354a999e308e347
sai srinivas <srinivas191194@msitprogram.net>
2020-07-20 17:16:02 +0530

    Jul 20  17:16:01

--- 02-hasnoprimes-Java/hasnoprimes.java
+++ 02-hasnoprimes-Java/hasnoprimes.java
@@ -4,6 +4,23 @@
 
 class hasnoprimes {
 	public boolean fun_hasnoprimes(int[][] arr){
+        for(int[] eachrow : arr){
+          for(int j : eachrow){
+			  if(isPrime(j)){
+				  return false;
+			  }
+		  }
+		}
+		return true;
+	}
+
+
+
+	public boolean isPrime(int x){
+		for(int i = 2; i < x-1; i++){
+			if(x%i == 0)
+			  return false;
+		}
 		return true;
 	}
 }
