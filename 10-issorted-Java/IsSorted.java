7194df1d16c2013ca641ba5955893ff550a366ac
sai srinivas <srinivas191194@msitprogram.net>
2020-07-23 15:31:03 +0530

    Jul 23  15:31:03

--- 10-issorted-Java/IsSorted.java
+++ 10-issorted-Java/IsSorted.java
@@ -4,17 +4,32 @@
 // must only consider each value in the list once (so, in terms of big-oh, which we will 
 // learn soon, it runs in O(n) time, where n=len(a)), and so in particular you may not sort 
 // the list.
-
+import java.util.*;
 public class IsSorted {
 	public boolean isSorted(double[] list) {
 		// Your code goes here
-
-
+		int s = list.length;
+		if(s == 1 || s == 0)
+		  return true;
+       if(list[0] < list[1]){
+		   for(int i = 1; i < list.length;i++){
+			   if(list[i] < list[i-1])
+			     return false;
+		   }
+		   return true;
+	   }
+	   if(list[0] > list[1]){
+		   for(int i = 1; i < list.length; i++){
+			   if(list[i] > list[i-1])
+			     return false;
+		   }
+		   return true;
+	   }
 
 
 		
 
-		
+
 
 		return false;
 	}
