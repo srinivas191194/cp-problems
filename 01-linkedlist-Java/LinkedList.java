b2a7f1f93f6cf375d02c3727d8ae39ba7e51df46
sai srinivas <srinivas191194@msitprogram.net>
2020-07-23 16:58:56 +0530

    done

--- 01-linkedlist-Java/LinkedList.java
+++ 01-linkedlist-Java/LinkedList.java
@@ -10,12 +10,27 @@ class Element{
 
 public class LinkedList{
 	Element head;
+	int size;
 	public LinkedList(Element head){
 		this.head = head;
 	}
 
 	public void append(Element new_element){
 		// Your code goes here
+		if(head == null){
+			head = new_element;
+			size++;
+		}
+		Element current = head;
+	    while(current != null){
+	         if(current.next == null){
+	             current.next = new_element;
+	             size++;
+	             break;
+	         }
+	         current = current.next;
+	    }
+	    
 	}
 
 	public Element get_position(int position){
@@ -23,6 +38,17 @@ public class LinkedList{
         // Assume the first position is "1".
         // Return null if position is not in the list
 		// Your code goes here
+		Element current = head;
+        int track = 0;
+		while(current!= null){
+			track = track+1;
+			if(track == position){
+			    System.out.println(current.value);
+				return current;
+			}
+			current = current.next;
+		}
+		
 		return null;
 	}
 
@@ -32,11 +58,46 @@ public class LinkedList{
        // Inserting at position 3 means between
        // the 2nd and 3rd elements."""
 		// Your code goes here
+	 if(position == 1){
+		 new_element.next  = head;
+		 head = new_element;
+		 size++;
+	 }
+	 Element prev = head;
+	 Element current = prev.next;
+	 int count = 1;
+	 while(current != null){
+		 count = count+1;
+		 if(count == position){
+			 prev.next = new_element;
+			 new_element.next = current;
+			 size++;
+			 break;
+		 }
+       prev = current;
+		 current = current.next;
+	 }
 
 	}
 
 	public void delete(int value){
 		// Delete the first node with a given value.
 		// Your code goes here
+		if(head.value == value){
+			head = head.next;
+			size--;
+		}
+		Element prev = head;
+		Element current = prev.next;
+		while(current != null){
+			if(current.value == value){
+			  prev.next = current.next;
+			  size++;
+			  break;
+			}
+			prev = current;
+			current =current.next;
+		}
 	}
 }
+
