9bdc15aba84175ed6972ad05672dbad0e48566a1
sai srinivas <srinivas191194@msitprogram.net>
2020-07-23 17:25:20 +0530

    done

--- 02-binarysearch-Java/BinarySearch.java
+++ 02-binarysearch-Java/BinarySearch.java
@@ -1,6 +1,17 @@
 class BinarySearch {
 	public int binary_search(int[] arr, int value){
         // Your code goes here
+       int start = 0;
+       int end = arr.length;
+        while(start <= end){
+                int mid = (start+end)/2;
+                if(arr[mid] == value)
+                   return mid;
+                else if(arr[mid] < value)
+                   start = mid +1;
+                else if(arr[mid] > value)
+                   end = mid-1;
+        }
         return -1;
 	}
 }
