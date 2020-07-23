b6873078367ee67d016124b6ac8a6249b8e6835f
sai srinivas <srinivas191194@msitprogram.net>
2020-07-23 21:57:49 +0530

    done

--- 03-quicksort-Java/QuickSort.java
+++ 03-quicksort-Java/QuickSort.java
@@ -1,6 +1,40 @@
+import java.util.*;
 class QuickSort {
 	public int[] quicksort(int[] arr){
 		// Your code goes here
-		return new int[0];
+		int start = 0;
+		int end  = arr.length-1;
+		quicksort1(arr,start,end);
+		return arr;
+	}
+	public void quicksort1(int[] arr, int start, int end){
+		if(start < end){
+			int r = partition(arr,start,end);
+			quicksort1(arr,start,r-1);
+			quicksort1(arr,r+1,end);
+		}
+	}
+	public int partition(int[] arr, int start, int end){
+		int pivot = arr[start];
+		int temp = start;
+		while(start < end){
+			while(arr[start] <= pivot){
+				start++;
+			}
+			while(arr[end] > pivot){
+				end--;
+			}
+			if(start < end){
+				swap(arr,start,end);
+			}
+		}
+		arr[temp] = arr[end];
+		arr[end] = pivot;
+		return end;
+	}
+	public void swap(int[] arr, int start, int end){
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
 	}
 }
