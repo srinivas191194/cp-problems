ed9100f18f05036bf9fab3965519abe8120ecb82
sai srinivas <srinivas191194@msitprogram.net>
2020-07-25 20:28:16 +0530

    done

--- 05-alternatingsum-Java/alternatingsum.java
+++ 05-alternatingsum-Java/alternatingsum.java
@@ -2,9 +2,29 @@
 // # alternating sum (where the sign alternates from positive to negative or vice versa). 
 // # For example, alternatingSum([5,3,8,4]) returns 6 (that is, 5-3+8-4). If the list is empty, return 0.
 
-
+import java.util.*;
 class alternatingsum {
 	public int fun_alternatingsum(int[] s){
-		return 0;
+		boolean flag1 = true;
+		// boolean flag2 = false;
+		int result = 0;
+		int len = s.length; 
+	    if(len > 0){
+			for(int i = 0; i < len; i++){
+				if(flag1 == true){
+					result += s[i];
+					flag1 = false;
+					// flag2 = true;
+				}
+				else{
+				   result += (-1*s[i]);
+				   flag1 = true;
+				}
+			}
+			return result;
+		}
+		else{
+			return result;
+		}
 	}
 }
