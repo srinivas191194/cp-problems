9b80a4c95402d61a9d51a3f489ee7096297e8dab
sai srinivas <srinivas191194@msitprogram.net>
2020-07-26 20:13:17 +0530

    Jul 26  20:13:17

--- 03-recursion_onlyevendigits-Java/recursion_onlyevendigits.java
+++ 03-recursion_onlyevendigits-Java/recursion_onlyevendigits.java
@@ -8,10 +8,53 @@
 // # Also the function returns the empty array if the original array is empty. 
 // # Remember to not use strings. You may not use loops/iteration in this problem.
 import java.lang.Math; 
-class recursion_onlyevendigits {
+import java.util.*;
+public class recursion_onlyevendigits {
 	public int[] fun_recursion_onlyevendigits(int[] l){
-		int[] arr = {};
-		return arr;
+		if(l.length == 0){
+		   int[] arr1 ={};
+		   return arr1;
+		}
+		ArrayList<Integer> obj = new ArrayList<Integer>();
+		recursion(l,0,obj);
+		int[] arr = new int[obj.size()];
+		for(int i = 0; i < obj.size(); i++)
+		   arr[i] = obj.get(i);
+		return arr;   
+	}
+	public void recursion(int[] l , int index, ArrayList<Integer> obj){
+		if(index >= l.length)
+		   return;
+		int result = 0;
+		int temp = l[index];
+		int placeValue = 1;
+// 		while(temp > 0){
+// 			int a = temp % 10;
+// 			if(a % 2 == 0){
+// 			  result = a*placeValue + result;
+// 			  placeValue =placeValue*10;
+// 			}
+// 			temp = temp/10;  
+// 		}
+        int value = recursion2(temp,placeValue,result);
+		obj.add(value);
+		recursion(l,index+1,obj);
+	}
+	public int recursion2(int number,int place,int result){
+	    if(number == 0)
+	      return result;
+	    int a = number % 10;
+	    if(a % 2 == 0){
+	      result = a*place + result;
+	      place = place*10;
+	    }
+	    number = number /10;
+	    return recursion2(number ,place,result);
+	}
+	public static void main(String[] args){
+		recursion_onlyevendigits   object = new recursion_onlyevendigits();
+		int[] arr ={43,23265,17,58344};
+ System.out.println(Arrays.toString(object.fun_recursion_onlyevendigits(arr)));
 	}
 }
 
