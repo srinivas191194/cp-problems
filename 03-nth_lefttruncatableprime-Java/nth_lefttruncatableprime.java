79aca5291f18d7e8b69f007c1a59219e75c88c0b
sai srinivas <srinivas191194@msitprogram.net>
2020-08-08 12:47:11 +0530

    done

--- 03-nth_lefttruncatableprime-Java/nth_lefttruncatableprime.java
+++ 03-nth_lefttruncatableprime-Java/nth_lefttruncatableprime.java
@@ -9,8 +9,53 @@
 
 
 class nth_lefttruncatableprime {
+	public boolean isPrime(int num,int i){
+		if(num <= 2){
+			return (num == 2) ? true : false;
+		}
+		if(num%i ==0){
+			return false;
+		}
+		if(i*i > num){
+			return true;
+		}
+		return isPrime(num,i+1);
+	}
+	public boolean isLeftTruncatable(int n){
+		int number = n;
+		int count = 0;
+		while(number > 0){
+			int t = number % 10;
+			count += 1;
+			if(t == 0){
+				return false;
+			}
+			number = number/10;
+		}
+		int power = (int) Math.pow(10,count);
+		number = n;
+		for(int i = 0; i < count; i++){
+			if(isPrime(number,2)){
+				power = power/10;
+				number = number%power;
+			}else{
+				return false;
+			}
+		}
+		return true;
+	}
 	public int fun_nth_lefttruncatableprime(int n){
-		return 0;
+		int count = -1;
+		int guess = 0;
+		int result = 0;
+		while(count < n){
+			guess +=1;
+			if(isLeftTruncatable(guess)){
+				count +=1;
+				result = guess;
+			}
+		}
+		return result;
 	}
 }
 
